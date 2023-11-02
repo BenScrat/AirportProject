abb5f4575e0fade2484af3d83f875f8d7932a070
Dylou-Code <ceprik.dydy@gmail.com>
2023-11-02 16:23:47 +0100

    Feat : Modal form with add avions

--- src/main/java/com/example/airportproject/ws/PassagerDTO.java
+++ src/main/java/com/example/airportproject/ws/PassagerDTO.java
@@ -1,6 +1,8 @@
 package com.example.airportproject.ws;
 
+import com.example.airportproject.bo.AvionViews;
 import com.example.airportproject.bo.Passager;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,11 +10,13 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonView(AvionViews.Extended.class)
 public class PassagerDTO {
 
     private String nom;
     private String prenom;
     private int age;
+
 
     public PassagerDTO(Passager passager) {
         this.nom = passager.getNom();
